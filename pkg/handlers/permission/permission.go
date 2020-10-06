b3b1e603c4dd50ab362e96af081b35a0e43aa085
luabish <wubw@pku.edu.cn>
2020-10-07 01:39:43 +0800

    feat: add job creation for permission handler

--- pkg/handlers/permission/permission.go
+++ pkg/handlers/permission/permission.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 	"github.com/bitnami-labs/kubewatch/config"
 	"github.com/bitnami-labs/kubewatch/pkg/event"
+	"github.com/bitnami-labs/kubewatch/pkg/utils"
+	batchV1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 type Permission struct {
@@ -12,7 +18,7 @@ type Permission struct {
 	chown string
 }
 
-func (p Permission) Init(c *config.Config) error {
+func (p *Permission) Init(c *config.Config) error {
 	p.ScName=c.Handler.Permission.ScName
 	p.chmod=c.Handler.Permission.Chmod
 	p.chown=c.Handler.Permission.Chown
@@ -21,7 +27,69 @@ func (p Permission) Init(c *config.Config) error {
 	return nil
 }
 
-func (p Permission) Handle(e event.Event)  {
-	fmt.Println("收到pv：",e)
-	fmt.Println(p.ScName,p.chmod,p.chown)
+func (p *Permission) Handle(e event.Event)  {
+	if e.Reason!="Created" || e.Kind!="persistent volume" {
+		//fmt.Println("跳过:",e.Reason,e.Kind)
+		return
+	}
+	//fmt.Println(p.ScName,p.chmod,p.chown)
+
+	var clientSet kubernetes.Interface
+
+	if _, err := rest.InClusterConfig(); err != nil {
+		clientSet = utils.GetClientOutOfCluster()
+	} else {
+		clientSet = utils.GetClient()
+	}
+
+	pv, _ := clientSet.CoreV1().PersistentVolumes().Get(e.Name, metaV1.GetOptions{})
+	fmt.Println(pv.Spec.ClaimRef.Name, pv.Spec.ClaimRef.Namespace, pv.Status.Phase)
+
+	newJob := &batchV1.Job{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name: fmt.Sprintf("pfixer-%s-%s",pv.Spec.ClaimRef.Name,pv.Spec.ClaimRef.Namespace),
+			Namespace: pv.Spec.ClaimRef.Namespace,
+			Labels: map[string]string{"jobgroup":"permission"},
+		},
+		Spec: batchV1.JobSpec{
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metaV1.ObjectMeta{
+					Name: fmt.Sprintf("pfixer-%s-%s",pv.Spec.ClaimRef.Name,pv.Spec.ClaimRef.Namespace),
+					Labels: map[string]string{"jobgroup":"permission"},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "permission-fixer",
+							Image: "alpine",
+							Command: []string{
+								"sh",
+								"-c",
+								fmt.Sprintf("chmod -R %s /data && chown -R %s:%s /data",p.chmod,p.chown,p.chown),
+							},
+							VolumeMounts: []v1.VolumeMount{
+								{
+									MountPath: "/data",
+									Name: "data",
+								},
+							},
+						},
+					},
+					RestartPolicy: v1.RestartPolicyNever,
+					Volumes: []v1.Volume{
+						{
+							Name: "data",
+							VolumeSource: v1.VolumeSource{
+								PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{
+									ClaimName: pv.Spec.ClaimRef.Name,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	result,_:=clientSet.BatchV1().Jobs(pv.Spec.ClaimRef.Namespace).Create(newJob)
+	fmt.Println(result)
 }
